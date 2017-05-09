a68a1bccb6c747c030d7f9ca98b677da8aca9844
realtimschmidt <schmidt.tim.j@gmail.com>
2017-05-09 13:00:46 -0400

    resolved 3 rspec tests

--- spec/controllers/advertisements_controller_spec.rb
+++ spec/controllers/advertisements_controller_spec.rb
@@ -48,18 +48,18 @@ RSpec.describe AdvertisementsController, type: :controller do
     end
   end
 
-  describe "ADVERTISEMENT create" do
+  describe "POST create" do
     it "increases the number of advertisement by 1" do
-      expect{advertisement :create, advertisement: {title: RandomData.random_sentence, body: RandomData.random_paragraph, price: 100}}.to change(Advertisement,:count).by(1)
+      expect{post :create, advertisement: {title: RandomData.random_sentence, body: RandomData.random_paragraph, price: 100}}.to change(Advertisement,:count).by(1)
     end
 
     it "assigns the new advertisement to @advertisement" do
-      advertisement :create, advertisement: {title: RandomData.random_sentence, body: RandomData.random_paragraph, price: 100}
+      post :create, advertisement: {title: RandomData.random_sentence, body: RandomData.random_paragraph, price: 100}
       expect(assigns(:advertisement)).to eq Advertisement.last
     end
 
     it "redirects to the new advertisement" do
-      advertisement :create, advertisement: {title: RandomData.random_sentence, body: RandomData.random_paragraph, price: 100}
+      post :create, advertisement: {title: RandomData.random_sentence, body: RandomData.random_paragraph, price: 100}
       expect(response).to redirect_to Advertisement.last
     end
   end
