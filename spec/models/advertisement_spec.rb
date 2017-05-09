514ae64c72a8bfef4fc3debf544f41262f2cbcc7
realtimschmidt <schmidt.tim.j@gmail.com>
2017-05-08 21:39:17 -0400

    wip Assignment 19 3 errors remain

--- spec/models/advertisement_spec.rb
+++ spec/models/advertisement_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Advertisement, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:advertisement) { Advertisement.create!(title: "New Post Title", body: "New Post Body", price: 100) }
+
+  describe "attributes" do
+    it "has title, body, and price attributes" do
+      expect(advertisement).to have_attributes(title: "New Post Title", body: "New Post Body", price: 100)
+    end
+  end
 end
