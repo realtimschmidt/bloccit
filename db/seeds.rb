49a182f581becf1395c0b2313a88196d7a7dabac
realtimschmidt <schmidt.tim.j@gmail.com>
2017-05-12 14:17:25 -0400

    Update

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,17 +25,11 @@ posts = Post.all
 end
 
 # post id: 51
-Post.find_or_create_by!(title: "New Title for a New Post", body: "New Body for a New Post")
-# post id: 52
-Post.find_or_create_by!(title: "Another new post title", body: "Another new body.")
-Post.find_or_create_by!(title: "So many titles!", body: "So many bodys!")
-Post.find_or_create_by!(title: "Just one more title", body: "Just one more body.")
+post_var = Post.find_or_create_by!(title: "New Title for a New Post", body: "New Body for a New Post")
 
 # comment id: 101 with post id:51
-Comment.find_or_create_by!(post: Post.find_or_create_by!(title: "New Title for a New Post", body: "New Body for a New Post"), body: "This is a unique comment body and it differs from the related post body")
+Comment.find_or_create_by!(post: post_var, body: "This is a unique comment body and it differs from the related post body")
 
-# comment id:102 with brand new post
-Comment.find_or_create_by!(post: Post.find_or_create_by!(title: "Post from a comment", body: "This post didn't exist before this comment was created"), body: "A very unique comment for a very unique post")
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
